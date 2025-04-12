8ea1f015727b1ff495f6fb7babe0801a2fe89901
Michael Orsi <Michaelorsibm@outlook.com>
2025-04-12 22:10:53 +0200

    ex017 de js

---
+++ exercicios/ex017/script.js
@@ -0,0 +1,27 @@
+
+function contar() { var ini = document.querySelector('input#txti')
+    var fim = document.querySelector('input#txtf')
+    var passo = document.querySelector('input#txtp')
+    var res = document.querySelector('div#res')
+    
+    if (ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
+        window.alert('[ERRO] faltam dados')
+    } else {
+        res.innerHTML = 'Contando'
+        var i = Number(ini.value)
+        var f = Number(fim.value)
+        var p = Number(passo.value)
+        if (i < f) {
+            // Contagem crescente
+            for(var c = 1; c <= f; c+= p) {
+                res.innerHTML += `${c}`
+            }
+        } else {
+            // Contagem regressiva
+            for(var c = i; c >= f; c -= p) {
+                res.innerHTML += `${c}`
+            }
+        }
+    }
+}
+   
