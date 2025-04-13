0ece92a38972f7dd446e34f83de1cb6c2ff31317
Michael Orsi <Michaelorsibm@outlook.com>
2025-04-13 17:55:55 +0200

    atualizei o exercício

--- exercicios/ex017/script.js
+++ exercicios/ex017/script.js
@@ -1,5 +1,5 @@
-
-function contar() { var ini = document.querySelector('input#txti')
+function contar() { 
+    var ini = document.querySelector('input#txti')
     var fim = document.querySelector('input#txtf')
     var passo = document.querySelector('input#txtp')
     var res = document.querySelector('div#res')
@@ -7,13 +7,13 @@ function contar() { var ini = document.querySelector('input#txti')
     if (ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
         window.alert('[ERRO] faltam dados')
     } else {
-        res.innerHTML = 'Contando'
+        res.innerHTML = 'Contando: '
         var i = Number(ini.value)
         var f = Number(fim.value)
         var p = Number(passo.value)
         if (i < f) {
             // Contagem crescente
-            for(var c = 1; c <= f; c+= p) {
+            for(var c = i; c <= f; c+= p) {
                 res.innerHTML += `${c}`
             }
         } else {
